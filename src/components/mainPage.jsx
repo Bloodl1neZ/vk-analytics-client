8fe307cc6a0524a8a644575caa18be1ac4038fe2
BloodL1neZ <shinigamixrodion@gmail.com>
2019-11-23 17:24:43 +0300

    initial commit with redirect

--- src/components/mainPage.jsx
+++ src/components/mainPage.jsx
@@ -8,10 +8,30 @@ class MainPage extends Component {
             '&response_type=token' +
             '&v=5.102' +
             '&scope=friends,photos,offline' +
-            '&redirect_uri=https://vk-analytics-app.herokuapp.com/';
+            '&redirect_uri=https://vk-analytics-app.herokuapp.com';
+
+        const url1 = 'https://oauth.vk.com/authorize' +
+            '?client_id=7184308' +
+            '&display=page' +
+            '&response_type=token' +
+            '&v=5.102' +
+            '&scope=friends,photos,offline' +
+            '&redirect_uri=http://vk-analytics-app.herokuapp.com';
+
+        const url2 = 'https://oauth.vk.com/authorize' +
+            '?client_id=7184308' +
+            '&display=page' +
+            '&response_type=token' +
+            '&v=5.102' +
+            '&scope=friends,photos,offline' +
+            '&redirect_uri=vk-analytics-app.herokuapp.com';
 
         return (
-            <a href={url}>log in</a>
+            <div>
+                <a href={url}>log in</a>
+                <a href={url1}>log in 1</a>
+                <a href={url2}>log in 2</a>
+            </div>
         );
     }
 }
